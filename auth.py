977dd948090ed2391ae5887a44e30ac7326b6760
bostang <bostangsteiitb2020@gmail.com>
2025-07-19 09:51:32 +0700

    comment load_dotenv() (untuk pengembangan local)

--- auth.py
+++ auth.py
@@ -20,7 +20,7 @@ from dotenv import load_dotenv
 import os
 
 # Muat variabel lingkungan dari .env
-load_dotenv()
+# load_dotenv()     # UNTUK PENGEMBANGAN SECARA LOKAL.
 
 # Konfigurasi diambil dari variabel lingkungan
 SECRET_KEY = os.getenv("JWT_SECRET_KEY")
